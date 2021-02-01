d955f94724e6019e72d75c2545b9f3a577a835d3
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-02-01 11:15:30 +0000

    Updates

---
+++ index.js
@@ -0,0 +1,57 @@
+// Global Variables
+
+let employeeID;
+
+let profile = {
+    firstName: "",
+    lastName: "",
+    jobTitle: "",
+    email: "",
+    department: "",
+    location: ""
+}
+
+$(document).ready(function() {
+    buildTable();
+})
+
+
+function buildTable() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'php/getAll.php', 
+        dataType: 'json',
+        success: function(data) {
+
+            var db = data.data;
+            console.log(db);
+            console.log('HELLP!');
+
+            var numberOfEntries = 0;
+
+            for (let i in db) {
+                appendEntry(db, i)
+                numberOfEntries++
+            }
+
+            $('#numberOfEntries').html(numberOfEntries)
+
+        }
+    })
+}
+
+function appendEntry(db, i, filterBy) {
+
+    $('#database tbody').append(`
+        <tr onclick="loadProfile(${JSON.stringify(db[i]).split('"').join("&quot;")})">
+            <th class="hideCell">${db[i].id}</th>
+            <td><b>${db[i].lastName}</b>, ${db[i].firstName}</td>
+            <td class=${(filterBy == "jobTitle") ? "" : "hideCell"}>${db[i].jobTitle}</td>
+            <td class="hideCell">${db[i].email}</td>
+            <td class=${(filterBy == "department") ? "" : "hideCell"}>${db[i].department}</td>
+            <td class=${(filterBy == "location") ? "" : "hideCell"}>${db[i].location}</td>
+        </tr>
+    `)
+
+}
