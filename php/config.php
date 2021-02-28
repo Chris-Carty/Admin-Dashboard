1c402ce06e58c5907751e701ee8e152e6a528a9c
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-02-28 16:29:28 +0000

    Update

--- php/config.php
+++ php/config.php
@@ -1,21 +1,21 @@
 <?php
 
-	//$cd_host = "localhost";
-	//$cd_port = 8080;
-	//$cd_socket = "";
+	$cd_host = "localhost";
+	$cd_port = 8080;
+	$cd_socket = "";
 
 	// Development
-	//$cd_user = "root";
-	//$cd_password = "";
-	//$cd_dbname = "companydirectory";
+	$cd_user = "root";
+	$cd_password = "";
+	$cd_dbname = "companydirectory";
 
 	// Production 
-	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$cleardb_server = $cleardb_url["host"];
-	$cleardb_username = $cleardb_url["user"];
-	$cleardb_password = $cleardb_url["pass"];
-	$cleardb_db = substr($cleardb_url["path"],1);
-	$active_group = 'default';
-	$query_builder = TRUE;
+	//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	//$cleardb_server = $cleardb_url["host"];
+	//$cleardb_username = $cleardb_url["user"];
+	//$cleardb_password = $cleardb_url["pass"];
+	//$cleardb_db = substr($cleardb_url["path"],1);
+	//$active_group = 'default';
+	//$query_builder = TRUE;
 
 ?>
